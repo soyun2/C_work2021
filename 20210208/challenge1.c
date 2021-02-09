68d0f46f44daa3d2d50e70ae1e4d21d31dd41967
soyun2 <tkdtod0619@naver.com>
2021-02-09 09:54:32 +0900

    challenge1풀이

--- 20210208/challenge1.c
+++ 20210208/challenge1.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-
-void turn(int *arr)
+/*
+void turn(int (*p)[4])
 {
-    int *p = arr;
     int brr[4][4] = {0,};
     int i,j,k;
 
@@ -11,23 +10,52 @@ void turn(int *arr)
         for(i=0;i<4;i++)
         {
             for(k=0;k<4;k++)
-            brr[j][i] = (arr[i]+k);
+            brr[j][i] = arr[i][k];
         }
+    }    
+}
+*/
+
+void doChange(int (*arr)[4], int (*brr)[4])
+{
+    int i, j;
+    int k = 3;
+    for(i = 0;i<4;i++)
+    {
+        for(int j=0; j<4; j++)
+        {
+            brr[j][k] = arr[i][j];
+        }
+        k--;
     }
+}
+
+void doPrint(int (*p)[4])
+{
+    int i, j;
 
     for(i=0;i<4;i++){
         for(j=0;j<4;j++)
         {
-            printf("%4d", brr[i][j]);
+            printf("%4d", p[i][j]);
         }
+        printf("\n");
     }
-    
 }
 
 void main()
 {
     int i,j;
     int arr[4][4] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
+    int brr[4][4];
 
-    turn(arr[0]);
+    doPrint(arr);
+    printf("\n");
+    doChange(arr,brr);
+    doPrint(brr);
+    printf("\n");
+    doChange(brr, arr);
+    doPrint(arr);
 }
+
+
